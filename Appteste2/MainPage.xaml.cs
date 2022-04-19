cfde704493bef10283507320844187ece62838fd
21022697 <21022697@302-09>
2022-04-18 22:10:53 -0300

    Adicionar arquivos de projeto.

---
+++ Appteste2/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Appteste2
+{
+    public partial class MainPage : ContentPage
+    {
+        static string sorte= new Random().Next(1,4).ToString();
+
+        async void Cliquei(object sender, EventArgs e)
+        {
+            Button botao = sender as Button;
+            if (botao.Text == sorte)
+            {
+                await DisplayAlert("Achou!", "Você é sortudo!", "Tente novamente!");
+                sorte = new Random().Next(1,4).ToString();
+            }
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
